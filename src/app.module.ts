d7a6dd434c131187f9c9f0b74383bf68b1aa5f40
RuslanSalimov <mail-spam2012@yandex.ru>
2025-04-24 18:02:25 +0500

    feat: Create Users & Tasks resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './tasks/users/users.module';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
